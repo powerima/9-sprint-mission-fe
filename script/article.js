d6a6d12beda664c628505bda5d1e5e263a2284bb
powerima <powerima@naver.com>
2025-09-14 02:20:03 +0900

    [feat] 미션 4 - article 목록 조회 getArticleList 구현

--- script/article.js
+++ script/article.js
@@ -10,8 +10,8 @@ import { URL_ARTICLE, DEFAULT_ARTICLE_OBJ, DEFAULT_PRODUCT_OBJ } from "./api_com
 import axios from 'axios';
 
 ( async () =>  {
-    const func = getArticleFetch;
-    const data = await func(12);
+    const func = createArticleFetch;
+    const data = await func();
     
     console.log('data = > ', func, data);
 })();
@@ -25,8 +25,8 @@ async function getArticle(id) {
 
 
 
-/* article 상세 조회 - GET Request 이용  */
-async function getArticleAxios(id=1) {
+/* article 상세 조회 - aios Get  */
+async function getArticleAxios(id = 1) {
     const URL = URL_ARTICLE + '/' + id;
     let data;
     
@@ -35,15 +35,16 @@ async function getArticleAxios(id=1) {
         console.log(res.data);
 
     }).catch((err) => {
-        console.log(err);        
+        console.log(err);      
+    
     });
 
     return data;
 }
 
 
-/* article 상세 조회 - GET Request 이용  */
-async function getArticleFetch(id=1) {
+/* article 상세 조회 - fetch get  */
+async function getArticleFetch(id = 1) {
     const URL = URL_ARTICLE + '/' + id;
     let data;
 
@@ -53,40 +54,64 @@ async function getArticleFetch(id=1) {
 
     } catch (err) {
         console.log(err);
+
     }
 
     return data;
 }
 
 
-/* article 목록 조회 - GET Request 이용  */
-async function getArticleListAxios(id=1) {
-    const data = await fetch(URL_ARTICLE)
-                .then().json();
+/* article 목록 조회 - orderby: 글 정렬 방식 recent/like, keyword: 검색 키워드  */
+async function getArticleList(pageNumber = 1, pageSize = 10, orderBy = 'recent', keyword='') {
+    getArticleListFetch(pageNumber = 1, pageSize = 10, orderBy = 'recent', keyword='');
+
+}
+
+
+/* article 목록 조회 - axios get  */
+async function getArticleListAxios(pageNumber = 1, pageSize = 10, orderBy = 'recent', keyword='') {
+    const URL = URL_ARTICLE + '?page=' + pageNumber + '&pageSize=' 
+                    + pageSize + '&orderBy=' + orderBy + '&keyword=' + keyword;
+    let data;
+
+    await axios.get(URL).then((res) => {
+        data = res.data;
+        console.log(data);
+
+    }).catch((err) => {
+        console.log(err);
+
+    });
 
     return data;
 }
 
 
-/* article 목록 조회 - GET Request 이용  */
-async function getArticleListFetch(id=1) {
-    const URL = URL_ARTICLE + '/' + id;
-    const res = await fetch(URL);
-    const data = await res.json();
-                
-    console.log(data);
+/* article 목록 조회 - fetch get  */
+async function getArticleListFetch(pageNumber = 1, pageSize = 10, orderBy = 'recent', keyword='') {
+    const URL = URL_ARTICLE + '?page=' + pageNumber + '&pageSize=' 
+                    + pageSize + '&orderBy=' + orderBy + '&keyword=' + keyword;
+    let data;
+    
+    try {
+        const res = await fetch(URL);
+        data = await res.json();
+        console.log(data);
+
+    } catch(err) {
+        console.log(err);
+
+    }
 
     return data;
 }
 
-/* article 목록 조회 - GET Request 이용  */
-async function getArticleList(id) {
-    getArticleListFetch(id);
-}
 
-/* article 생성 - POST Request 이용  */
+
+/* article 생성 - { image: 'imgurl', content: 본문, title: 제목 } */
 async function createArticle(obj) {
-    const res = await fetch(URL, {
+    const URL = URL_ARTICLE;
+    const res = await axios.get(URL, {
         mothod: 'POST',
         body: JSON.stringyfy(obj),
         headers: {
@@ -96,6 +121,59 @@ async function createArticle(obj) {
 
     return;
 }
+
+
+
+/* article 생성 - axiod post    */
+async function createArticleAxios(obj = DEFAULT_ARTICLE_OBJ) {
+    const URL = URL_ARTICLE;
+    let data;
+
+    try {
+        const res = await fetch(URL, {
+            mothod: 'POST',
+            body: JSON.stringify(obj),
+            headers: {
+                'Content-type': 'application/json',
+            },
+        });
+
+        console.log('res = > ', res);
+
+    } catch(err) {
+        console.log(err);
+
+    }
+
+    return;
+}
+
+
+/* article 생성 - fetch post    */
+async function createArticleFetch(obj = DEFAULT_ARTICLE_OBJ) {
+    const URL = URL_ARTICLE;
+    let data;
+
+    try {console.log('createArticleFetch function call - ');
+        const res = await fetch(URL, {
+            mothod: 'POST',
+            body: JSON.stringify(obj),
+            headers: {
+                'Content-type': 'application/json',
+            },
+        });
+
+        console.log('res = > ', res);
+
+    } catch(err) {
+        console.log(err);
+
+    }
+
+
+    return;
+}
+
 
 /* article 삭제 - DELETE Request 이용 */
 async function deleteArticle(id) {
