6613e712c20b0658313a7e36a2ba9e5a77630ae6
powerima <powerima@naver.com>
2025-09-14 01:25:50 +0900

    [feat] 미션 4 - article 상세 조회 getArticle 구현

---
+++ script/api_common.js
@@ -0,0 +1,29 @@
+/*
+    2025. 09. 04
+
+    api CRUD 관련 공통 함수 및 변수
+
+    김유신
+*/
+
+export const URL_ARTICLE= 'https://panda-market-api-crud.vercel.app/articles';
+export const URL_PRODUCT = 'https://panda-market-api-crud.vercel.app/products';
+export const RANDOM_IMAGE_URL = 'https://cdn.pixabay.com/photo/2025/04/24/22/36/beach-9556784_1280.jpg';
+
+export const DEFAULT_ARTICLE_OBJ = {
+    "image": RANDOM_IMAGE_URL,
+    "content": "게시글 내용입니다.",
+    "title": "게시글 제목입니다."
+};
+
+export const DEFAULT_PRODUCT_OBJ = {
+    "images": [
+        RANDOM_IMAGE_URL
+    ],
+    "tags": [
+        "전자제품"
+    ],
+    "price": 0,
+    "description": "string",
+    "name": "상품이름"
+};
--- script/article.js
+++ script/article.js
@@ -6,27 +6,87 @@
 */
 
 
-import { URL_ARTICLE, DEFAULT_ARTICLE_OBJ } from "./api_common";
+import { URL_ARTICLE, DEFAULT_ARTICLE_OBJ, DEFAULT_PRODUCT_OBJ } from "./api_common.js";
+import axios from 'axios';
 
-/* article 목록 조회 - GET Request 이용  */
-async function getArticleList(pageNum=1, pageSize=10, order='recent') {
-    const res = await fetch(`${URL_ARTICLE}?page=${pageNum}&pageSize=${pageSize}&orderBy=${order}`);
-    const data = await res.json();
+( async () =>  {
+    const func = getArticleFetch;
+    const data = await func(12);
+    
+    console.log('data = > ', func, data);
+})();
+
+
+/* article 상세 조회 */
+async function getArticle(id) {
+    return getArticleFetch(id);
+
+}
+
+
+
+/* article 상세 조회 - GET Request 이용  */
+async function getArticleAxios(id=1) {
+    const URL = URL_ARTICLE + '/' + id;
+    let data;
+    
+    await axios.get(URL).then((res) => {
+        data = res.data;
+        console.log(res.data);
+
+    }).catch((err) => {
+        console.log(err);        
+    });
 
     return data;
 }
 
-/* article 조회 - GET Request 이용  */
-async function getArticle(id=1) {
-    const res = await fetch(`${URL_ARTICLE}/${id}`);
-    const data = await res.json();
+
+/* article 상세 조회 - GET Request 이용  */
+async function getArticleFetch(id=1) {
+    const URL = URL_ARTICLE + '/' + id;
+    let data;
+
+    try {
+        const res = await fetch(URL);
+        data = await res.json();
+
+    } catch (err) {
+        console.log(err);
+    }
 
     return data;
+}
+
+
+/* article 목록 조회 - GET Request 이용  */
+async function getArticleListAxios(id=1) {
+    const data = await fetch(URL_ARTICLE)
+                .then().json();
+
+    return data;
+}
+
+
+/* article 목록 조회 - GET Request 이용  */
+async function getArticleListFetch(id=1) {
+    const URL = URL_ARTICLE + '/' + id;
+    const res = await fetch(URL);
+    const data = await res.json();
+                
+    console.log(data);
+
+    return data;
+}
+
+/* article 목록 조회 - GET Request 이용  */
+async function getArticleList(id) {
+    getArticleListFetch(id);
 }
 
 /* article 생성 - POST Request 이용  */
-async function createArticle(obj= DEFAULT_ARTICLE_OBJ) {
-    const res = await fetch(URL_ARTICLE, {
+async function createArticle(obj) {
+    const res = await fetch(URL, {
         mothod: 'POST',
         body: JSON.stringyfy(obj),
         headers: {
