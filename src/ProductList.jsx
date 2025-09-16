673e99ff04adc14dc84949cb8309e51460d7bb4d
powerima <powerima@naver.com>
2025-09-16 16:42:48 +0900

    [feat] 미션 5 - 판다마켓 리액트 상품리스트 컴포넌트 구현

---
+++ src/ProductList.jsx
@@ -0,0 +1,32 @@
+/*
+    ProductList.jsx
+
+    2025. 09. 16
+
+    상품 리스트를 화면에 그리는 컴포넌트
+*/
+import styles from './ProductList.module.css'
+
+function ProductList( {productList} ) {
+  
+  const handleSortByName = () => {
+
+  };
+
+  return (
+    <div >
+      <ul className={styles.productList}>
+        {productList.map((el) => (
+          <li key={el.id} className={styles.productItem} >
+            <div>
+              <img className={styles.productThumbnail} src={el.images[0]} />
+              <strong>{el.description}</strong>
+              <span>({el.price})</span>              
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default ProductList;
