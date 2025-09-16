b9e9af07a92e33f4931c1059dea87a33d601c785
powerima <powerima@naver.com>
2025-09-16 16:44:16 +0900

    [feat] 미션 5 -판매중인 상품 리스트 구현

---
+++ src/ProductForSaleList.jsx
@@ -0,0 +1,25 @@
+import ProductList from "./ProductList";
+import styles from "./ProductForSaleList.module.css";
+
+function ProductForSaleList ({ productList }) {
+
+
+  return (
+    <>
+      <div className={styles.gnb}>
+        <span>판매중인 상품</span>
+        <div className={styles.listOptionContainer}>
+          <input className={styles.searchInput} placeholder="검색할 상품을 입력해주세요"/>
+          <button className={styles.regBtn}>상품 등록하기</button>
+          <select className={styles.orderSelector}>
+            <option>최신순</option>
+            <option>좋아요</option>
+          </select>
+        </div>
+      </div>
+      <ProductList productList={productList} />
+    </>
+  );
+}
+
+export default ProductForSaleList;
