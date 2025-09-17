7dc323c0cda3904f61143ed20d271df9d43970a0
powerima <powerima@naver.com>
2025-09-17 10:15:27 +0900

    [design] 미션 5 - 판매 중 리스트 컴포넌트의 검색 입력 창에 돋보기 아이콘 추가

--- src/ProductForSaleList.jsx
+++ src/ProductForSaleList.jsx
@@ -1,6 +1,6 @@
 import ProductList from "./ProductList";
 import styles from "./ProductForSaleList.module.css";
-
+import searchInputIcon from "./assets/search.svg";
 function ProductForSaleList ({ productList }) {
 
 
@@ -9,7 +9,10 @@ function ProductForSaleList ({ productList }) {
       <div className={styles.gnb}>
         <span>판매중인 상품</span>
         <div className={styles.listOptionContainer}>
-          <input className={styles.searchInput} placeholder="검색할 상품을 입력해주세요"/>
+          <div className={styles.searchInputWrap}>
+            <input className={styles.searchInput} placeholder="검색할 상품을 입력해주세요"/>
+            <img className={styles.searchInputIcon} src={searchInputIcon}/>
+          </div>
           <button className={styles.regBtn}>상품 등록하기</button>
           <select className={styles.orderSelector}>
             <option>최신순</option>
