28b570196c7941fa2fcc976db80b6278b011294c
powerima <powerima@naver.com>
2025-09-21 08:41:17 +0900

    [chore] 미션 5 - pate footer 파일 생성

---
+++ src/PageFooter.jsx
@@ -0,0 +1,22 @@
+/*
+  PageFooter.jsx
+
+  2025. 09. 16
+
+  팬더마켓 웹 페이지 하단의 풋터를 구현한 컴포넌트
+
+*/
+
+import styles from './PageFooter.module.css';
+
+
+function PageFooter() {
+
+  return (
+    <>
+    </>
+  );
+
+}
+
+export default PageFooter;
