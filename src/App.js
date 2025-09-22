a7cc4c358a231e27b0c7a3547687f1a261a834bd
powerima <powerima@naver.com>
2025-09-22 14:08:15 +0900

    [feat] 미션 5 - 판다마켓 중고 마켓 페이지 기능 구현

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import MarketPage from "./pages/MarketPage/MarketPage";
+import Header from "./components/Layout/Header";
+
+function App() {
+  return (
+    <>
+      {/* Global Navigation Bar */}
+      <Header />
+
+      <div className="withHeader">
+        <MarketPage />
+      </div>
+    </>
+  );
+}
+
+export default App;
